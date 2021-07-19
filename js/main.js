10872be85c90e69de4f670817afbcc8e42bed9b2
Victor Moak <42774915+VictorMoak@users.noreply.github.com>
2021-07-18 23:50:55 -0300

    LandingPage Moak

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
+
+$('.slider-prod').slick({
+    
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
